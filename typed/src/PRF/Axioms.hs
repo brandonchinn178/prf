b5b4d76378ccca619db456795c1d663b39fb91a6
Brandon Chinn <brandonchinn178@gmail.com>
2023-09-16 15:23:33 -0700

    [ci skip] wip

--- typed/src/PRF/Axioms.hs
+++ typed/src/PRF/Axioms.hs
@@ -37,39 +37,12 @@ module PRF.Axioms (
   p_4_4,
 ) where
 
-import Data.Kind (Constraint)
 import Data.Maybe (listToMaybe)
-import Data.Type.Bool
-import Data.Type.Equality
-import Data.Type.Ord
+import Data.Vector.Fixed qualified as Vec
+import Data.Vector.Fixed.Boxed (Vec)
 import GHC.Stack (HasCallStack)
 import GHC.TypeLits
 import PRF.Peano as X
-
-{----- Vec -----}
-
-data Vec (n :: Nat) a where
-  Nil :: Vec 0 a
-  (:&) :: a -> Vec n a -> Vec (n + 1) a
-
-infixr 6 :&
-
-type family IsLTImpl i n where
-  _ `IsLTImpl` 0 = False
-  0 `IsLTImpl` _ = True
-  i `IsLTImpl` n = (i <? n) && ((i - 1) `IsLTImpl` (n - 1))
-
-type i `IsLT` n = (IsLTImpl i n ~ True)
-
-type family IsLTEImpl i n where
-  0 `IsLTEImpl` 0 = True
-  i `IsLTEImpl` n = (i <=? n) && ((i - 1) `IsLTEImpl` (n - 1))
-
-type i `IsLTE` n = (IsLTEImpl i n ~ True)
-
-ix :: (1 `IsLTE` n, i `IsLT` n) => PN i -> Vec n a -> a
-ix Z (a :& _) = a
-ix (S i) (_ :& as) = ix i as
 
 {----- Func -----}
 
@@ -90,10 +63,12 @@ c :: PN k -> PN n -> Func k
 c k n = Func k $ \_ -> SomePN n
 
 s :: Func 1
-s = Func (S Z) $ \(SomePN n :& Nil) -> SomePN (S n)
+s = Func (S Z) $ \v ->
+  case Vec.convert v of
+    Vec.Only (SomePN n) -> SomePN (S n)
 
-p :: (1 <= i, 1 <= k, i <= k, i - 1 < k) => PN k -> PN i -> Func k
-p k (S n) = Func k $ ix n
+p :: (KnownNat k, i <= k, i ~ i0 + 1, i0 + 1 <= k) => PN k -> PN i -> Func k
+p k (S n) = Func k $ (`Vec.index` n)
 
 -- (•) :: Func -> [Func] -> Func
 -- h • gs
